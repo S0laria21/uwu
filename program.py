8e272933cee0261aca4a3cae0a0e5d8a974f1237
S0laria21 <marinnefoliaco@gmail.com>
2024-09-10 21:25:56 -0500

    Update program.py

--- program.py
+++ program.py
@@ -1,13 +1,14 @@
 import discord
+import random
+import os
 from bot_logic import gen_pass
+from bot_logic import get_duck_image_url
 from discord.ext import commands
-
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-bot = commands.Bot(commands_prifix = "$",intents=intents)
-
+bot = commands.Bot(command_prefix = "$",intents=intents)
 
 @bot.event
 async def on_ready():
@@ -35,7 +36,22 @@ async def joined(ctx, member: discord.Member):
     """Says when a member joined."""
     await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
 
+@bot.command()
+async def mem(ctx):
+    with open('images/mem1.jpg', 'rb') as f:
+        # ¡Vamos a almacenar el archivo de la biblioteca Discord convertido en esta variable!
+        picture = discord.File(f)
+    # A continuación, podemos enviar este archivo como parámetro.
+    await ctx.send(file=picture)
 
 @bot.command()
-async def bye(ctx, count_heh = 5):
-    await ctx.send("he" * count_heh)
+async def mem_ale(ctx):
+    mem_alet = random.choice(os.listdir("images"))
+    with open(f'images/{mem_alet}', 'rb') as f:
+        picture = discord.File(f)
+    await ctx.send(file=picture)
+
+@bot.command('duck')
+async def duck(ctx):
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
